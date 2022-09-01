f746a6ac6dfac42cb8d95b51f5237cd3a5747b27
myeongki.cho <myeongki.cho@navercorp.com>
2022-09-01 23:11:12 +0900

    db 연결

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
+
 bootstrap();
